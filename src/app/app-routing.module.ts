2d3f8b4594c931d42aaa9174baf94f4955cb183f
unknown <nilso80@gmail.com>
2023-05-03 12:32:52 -0300

    'update'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OSComponent } from './os/os.component';
 import { MenuComponent } from './menu/menu.component';
-import { TesteComponent } from './teste/teste.component';
 
 const routes: Routes = [
   { path: '', component: MenuComponent },
-  { path: 'os', component: OSComponent },
-  { path: 'teste', component: TesteComponent }
+  { path: 'os', component: OSComponent }
 ];
 
 @NgModule({
